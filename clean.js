0b953525a9f7f15ae7a48bc3536824db96ec2e68
trxizaz <141936934+trxizaz@users.noreply.github.com>
2023-08-29 18:46:18 +0530

    Create clean.js

---
+++ clean.js
@@ -0,0 +1,1 @@
+<script>/*<![CDATA[*/ var uri = window.location.toString();if (uri.indexOf("?m=1","?m=1") > 0) {var clean_uri = uri.substring(0, uri.indexOf("?m=1"));window.history.replaceState({}, document.title, clean_uri); }; /*]]>*/</script>
